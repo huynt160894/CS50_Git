4d077abe5c2a6767681aecbb183645266ab2fea8
huynt160894 <37900753+huynt160894@users.noreply.github.com>
2018-04-18 18:32:37 +0700

    Update application.py
    Adding headline variable

--- application.py
+++ application.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-  return render_template("index.html")
+  headline = "Hello, world!"
+  return render_template("index.html", headline=headline)
 
 @app.route("/<string:name>")
 def hello(name):
