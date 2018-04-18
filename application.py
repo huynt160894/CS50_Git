7e60263a20a53c5db3f4212c90d2b65493ff6119
huynt160894 <37900753+huynt160894@users.noreply.github.com>
2018-04-18 18:59:27 +0700

    Update application.py
    add isItNewYear()

--- application.py
+++ application.py
@@ -1,3 +1,5 @@
+import datatime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -11,6 +13,12 @@ def index():
 def bye():
   headline = "Goodbye!"
   return render_template("index.html", headline=headline)
+
+@app.route("/isitnewyear")
+def isItNewYear():
+  now = datetime.datetime.now()
+  new_year = now.month() == 1 and now.day == 1
+  return render_template("index.html", new_year = new_year)
 
 @app.route("/<string:name>")
 def hello(name):
